24ecc87da58c956f6051f235af07ca0dad9bc79c
ac-cool-breeze <alexcolter@gmail.com>
2021-06-21 11:18:10 -0500

    wrote two more tests, one for clicking, and another for clicking again

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,7 +1,23 @@
+import { wait } from "@testing-library/react"
+
 describe('Home component', ()=>{
     it('should show some newsCards', ()=>{
         cy.visit('http://localhost:3000')
         cy.get('[data-testid="test-newscard"]')
         .should('have.class','newsCard')
+    })
+
+    it('should change pin toggle when clicked on', ()=>{
+        let card = cy.get('[data-testid="test-newscard"]:first')
+        .click()
+        cy.get('img[alt="a pin to save article"]').should('have.class', 'Pinned')
+    })
+
+    it('should also change toggle back to unclicked when clicked on again',()=>{
+        let card = cy.get('[data-testid="test-newscard"]:first')
+        card.click()
+        card.within(()=>{
+            cy.get("img").should('have.class', 'notPinned')
+        })
     })
 })
